5b75b0578a8cdbf73f974c25d350f02204e1d9aa
Ray Lam <ray.lam@oocl.com>
2020-11-30 17:17:34 +0800

    [WordFrequencyRefactor][Ray Lam] refactor: extract method to simplify code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,25 +1,17 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
-
     private static final String WHITE_SPACE_REGEX = "\\s+";
     private static final int COUNT_ONE_WORD = 1;
     private static final String WORD_FREQUENCY_RESULT_PLUS_1 = " 1";
 
     public String getResult(String sentence) {
-
         if (sentence.split(WHITE_SPACE_REGEX).length == COUNT_ONE_WORD) {
             return sentence + WORD_FREQUENCY_RESULT_PLUS_1;
         } else {
             try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(WHITE_SPACE_REGEX);
-
-                List<WordFrequency> inputList = new ArrayList<>();
-                for (String word : words) {
-                    WordFrequency input = new WordFrequency(word, COUNT_ONE_WORD);
-                    inputList.add(input);
-                }
+                List<WordFrequency> inputList = getWordFrequencies(sentence);
 
                 //get the map for the next step of sizing the same word
                 Map<String, List<WordFrequency>> map = getListMap(inputList);
@@ -33,16 +25,38 @@ public class WordFrequencyGame {
 
                 inputList.sort((firstWord, secondWord) -> secondWord.getCount() - firstWord.getCount());
 
-                StringJoiner wordFrequencyResultJoiner = new StringJoiner("\n");
-                for (WordFrequency w : inputList) {
-                    String s = w.getWord() + " " + w.getCount();
-                    wordFrequencyResultJoiner.add(s);
-                }
-                return wordFrequencyResultJoiner.toString();
+                return buildWordFrequencyResult(inputList);
             } catch (Exception exception) {
                 return "Calculate Error";
             }
         }
+    }
+
+    public String buildWordFrequencyResult(List<WordFrequency> inputList) {
+        StringJoiner wordFrequencyResultJoiner = new StringJoiner("\n");
+        for (WordFrequency w : inputList) {
+            String s = w.getWord() + " " + w.getCount();
+            wordFrequencyResultJoiner.add(s);
+        }
+        return wordFrequencyResultJoiner.toString();
+    }
+
+    public List<WordFrequency> getWordFrequencies(String sentence) {
+        //split the input string with 1 to n pieces of spaces
+        String[] words = sentence.split(WHITE_SPACE_REGEX);
+
+        List<WordFrequency> inputList = new ArrayList<>();
+        for (String word : words) {
+            WordFrequency input = new WordFrequency(word, COUNT_ONE_WORD);
+            inputList.add(input);
+        }
+        return inputList;
+    }
+
+    public List<WordFrequency> getWordFrequencies_tmp(String sentence) {
+
+
+        return null;
     }
 
     private Map<String, List<WordFrequency>> getListMap(List<WordFrequency> inputList) {
