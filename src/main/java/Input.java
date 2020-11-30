f5d1d557864ba170d531b5fc8d64ca3200d79cb3
Ray Lam <ray.lam@oocl.com>
2020-11-30 17:04:37 +0800

    [WordFrequencyRefactor][Ray Lam] refactor: rename var and methods

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,17 +1,17 @@
 public class Input {
-    private String value;
+    private String word;
     private int count;
 
-    public Input(String w, int i) {
-        this.value = w;
-        this.count = i;
+    public Input(String word, int count) {
+        this.word = word;
+        this.count = count;
     }
 
-    public String getValue() {
-        return this.value;
+    public String getWord() {
+        return this.word;
     }
 
-    public int getWordCount() {
+    public int getCount() {
         return this.count;
     }
 }
--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,18 +6,18 @@ public class WordFrequencyGame {
     private static final int COUNT_ONE_WORD = 1;
     private static final String WORD_FREQUENCY_RESULT_PLUS_1 = " 1";
 
-    public String getResult(String inputStr) {
+    public String getResult(String sentence) {
 
-        if (inputStr.split(WHITE_SPACE_REGEX).length == COUNT_ONE_WORD) {
-            return inputStr + WORD_FREQUENCY_RESULT_PLUS_1;
+        if (sentence.split(WHITE_SPACE_REGEX).length == COUNT_ONE_WORD) {
+            return sentence + WORD_FREQUENCY_RESULT_PLUS_1;
         } else {
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split(WHITE_SPACE_REGEX);
+                String[] words = sentence.split(WHITE_SPACE_REGEX);
 
                 List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, COUNT_ONE_WORD);
+                for (String word : words) {
+                    Input input = new Input(word, COUNT_ONE_WORD);
                     inputList.add(input);
                 }
 
@@ -31,32 +31,32 @@ public class WordFrequencyGame {
                 }
                 inputList = list;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                inputList.sort((firstWord, secondWord) -> secondWord.getCount() - firstWord.getCount());
 
-                StringJoiner joiner = new StringJoiner("\n");
+                StringJoiner wordFrequencyResultJoiner = new StringJoiner("\n");
                 for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
+                    String s = w.getWord() + " " + w.getCount();
+                    wordFrequencyResultJoiner.add(s);
                 }
-                return joiner.toString();
-            } catch (Exception e) {
+                return wordFrequencyResultJoiner.toString();
+            } catch (Exception exception) {
                 return "Calculate Error";
             }
         }
     }
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
+        Map<String, List<Input>> wordCountMapping = new HashMap<>();
         for (Input input : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+            if (!wordCountMapping.containsKey(input.getWord())) {
+                ArrayList words = new ArrayList<>();
+                words.add(input);
+                wordCountMapping.put(input.getWord(), words);
             } else {
-                map.get(input.getValue()).add(input);
+                wordCountMapping.get(input.getWord()).add(input);
             }
         }
-        return map;
+        return wordCountMapping;
     }
 }
